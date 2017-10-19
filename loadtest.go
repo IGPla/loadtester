e0aaf58d6f09821666dba1d3dbd50759ed000c95
= <=>
2017-10-19 17:57:44 +0200

    Refactored code. Using waitgroup to synchronize all workers

--- loadtest.go
+++ loadtest.go
@@ -1,17 +1,20 @@
 package main
-/* Simple program that receives a single url, a workload and a max number of workers and will test performance with a concurrent approach */
+/* Simple program that receives a single url, number of jobs and a max number of workers and will test performance with a concurrent approach */
 
-import "fmt"
-import "time"
-import "flag"
-import "net/http"
+import (
+	"fmt"
+	"time"
+	"flag"
+	"sync"
+	"net/http"
+)
 
-func load_test(url string, ready chan bool, result chan uint64, closed chan bool) {
-	for true{
-		_, more := <- ready
-		if(!more){
-			break
-		}
+var waitgroup sync.WaitGroup
+
+func load_test(work chan string, result chan uint64) {
+	waitgroup.Add(1)
+	defer waitgroup.Done()
+	for url := range work {
 		t1 := time.Now()
 		_, err := http.Get(url)
 		if err != nil{
@@ -20,44 +23,45 @@ func load_test(url string, ready chan bool, result chan uint64, closed chan bool
 		t2 := time.Now()
 		result <- uint64(t2.Sub(t1).Nanoseconds())
 	}
-	closed <- true
 }
 
 func parse_flags() (string, uint64, uint64) {
 	/* Parse input flags to parametrize execution */
-	url := flag.String("url", "test", "This is the url that will be tested")
-	workload := flag.Uint64("workload", 10, "Workload for the url")
+	url := flag.String("url", "https://google.com", "This is the url that will be tested")
+	jobs := flag.Uint64("jobs", 10, "Jobs assigned to the url")
 	workers := flag.Uint64("workers", 2, "Concurrent workers")
 	flag.Parse()
-	return *url, *workload, *workers
+	return *url, *jobs, *workers
 }
 
 func main(){
-	url, workload, workers := parse_flags()
-	ready := make(chan bool, workload)
-	result := make(chan uint64, workload)
-	closed := make(chan bool, workers)
-
+	url, jobs, workers := parse_flags()
+	work := make(chan string, jobs)
+	result := make(chan uint64, jobs)
+	
 	var i uint64
+	
 	// Launch workers
 	for i = 0; i < workers; i++ {
-		go load_test(url, ready, result, closed)
+		go load_test(work, result)
 	}
+	
 	// Start work
-	for i = 0; i < workload; i++ {
-		ready <- true
+	for i = 0; i < jobs; i++ {
+		work <- url
 	}
+	
 	// Get results
 	var accum uint64 = 0
-	for i = 0; i < workload; i++ {
+	for i = 0; i < jobs; i++ {
 		accum += <- result
 	}
+	
 	// Close channels
-	close(ready)
+	close(work)
 	close(result)
 	
-	for i = 0; i < workers; i++{
-		<- closed
-	}
-	fmt.Printf("Average time for %s (%d workers with %d workload): %f\n", url, workers, workload, (float64(accum) / float64(workload)) / float64(1e9))
+	waitgroup.Wait()
+	
+	fmt.Printf("Average time for %s (%d workers for %d jobs): %f\n", url, workers, jobs, (float64(accum) / float64(jobs)) / float64(1e9))
 }
